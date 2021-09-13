808f84181df845f0ca39ba81560225cd2206b58e
Cefn Hoile <github.com@cefn.com>
2021-09-13 01:36:35 +0100

    Remove unnecessary nesting

---
+++ src/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="counter">
+      <h1>{{ count }}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { createStore } from "@lauf/store";
+import { createOptions } from "./lib/lauf-store-vue";
+
+const store = createStore({
+  count: 0,
+});
+
+setInterval(() => {
+  store.edit((state) => state.count++);
+}, 1000);
+
+export default Vue.extend({
+  name: "App",
+  ...createOptions(store, {
+    count: (state) => state.count,
+  }),
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
+import Counter from "./Counter.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  render: (h) => h(Counter),
 }).$mount("#app");
